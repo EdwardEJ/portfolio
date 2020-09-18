fff0bcd34b3f3fcb1ba854875d0735c4a6ec8dd7
A <jesus.e.estrada@hotmail.com>
2020-09-17 23:14:32 -0500

    added links and img folder

--- src/App.js
+++ src/App.js
@@ -1,56 +1,37 @@
-import React from 'react';
-import { useEffect, useState } from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
-  const [date, setDate] = useState(null);
-  useEffect(() => {
-    async function getDate() {
-      const res = await fetch('/api/date');
-      const newDate = await res.text();
-      setDate(newDate);
-    }
-    getDate();
-  }, []);
   return (
-    <main>
-      <h1>Create React App + Go API</h1>
-      <h2>
-        Deployed with{' '}
-        <a
-          href="https://vercel.com/docs"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          Vercel
-        </a>
-        !
-      </h2>
-      <p>
-        <a
-          href="https://github.com/vercel/vercel/tree/master/examples/create-react-app"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          This project
-        </a>{' '}
-        was bootstrapped with{' '}
-        <a href="https://facebook.github.io/create-react-app/">
-          Create React App
-        </a>{' '}
-        and contains three directories, <code>/public</code> for static assets,{' '}
-        <code>/src</code> for components and content, and <code>/api</code>{' '}
-        which contains a serverless <a href="https://golang.org/">Go</a>{' '}
-        function. See{' '}
-        <a href="/api/date">
-          <code>api/date</code> for the Date API with Go
-        </a>
-        .
-      </p>
-      <br />
-      <h2>The date according to Go is:</h2>
-      <p>{date ? date : 'Loading date...'}</p>
-    </main>
+    <>
+      <header>
+        <h1>Edward Estrada</h1>
+        <h2>Front End Developer</h2>
+        <p>
+          Hi I'm a web designer / developer based in Dallas, Texas. I have a
+          passion for web design and love to create for web and mobile devices
+          with the goal to convey the message that you want to send.
+        </p>
+      </header>
+      <main>
+        <section>
+          <h2>Projects</h2>
+          <div>
+            <a href="https://ghibli-studio-movie-app.herokuapp.com/">
+              Ghibli Studio Movie App
+            </a>
+            <a href="https://nasa-solo-project.netlify.app/">NASA APoD</a>
+          </div>
+        </section>
+        <section>
+          <h2>Skills</h2>
+          <img
+            src="src/imgs/css3-plain-wordmark.svg"
+            alt="CSS3 plain wordmark"
+          />
+        </section>
+      </main>
+    </>
   );
 }
 
