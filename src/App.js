e1ea9a9726faf5427e8a664c1801df4194d876da
A <jesus.e.estrada@hotmail.com>
2020-09-18 00:36:57 -0500

    initial html layout

--- src/App.js
+++ src/App.js
@@ -7,14 +7,16 @@ function App() {
       <header>
         <h1>Edward Estrada</h1>
         <h2>Front End Developer</h2>
-        <p>
-          Hi I'm a web designer / developer based in Dallas, Texas. I have a
-          passion for web design and love to create for web and mobile devices
-          with the goal to convey the message that you want to send.
-        </p>
+        <div>
+          <p>
+            Hi I'm a web designer / developer based in Dallas, Texas. I have a
+            passion for web design and love to create for web and mobile devices
+            with the goal to convey the message that you want to send.
+          </p>
+        </div>
       </header>
       <main>
-        <section>
+        <section id="projects">
           <h2>Projects</h2>
           <div>
             <a href="https://ghibli-studio-movie-app.herokuapp.com/">
@@ -23,14 +25,38 @@ function App() {
             <a href="https://nasa-solo-project.netlify.app/">NASA APoD</a>
           </div>
         </section>
-        <section>
+        <section id="skills">
           <h2>Skills</h2>
-          <img
-            src="src/imgs/css3-plain-wordmark.svg"
-            alt="CSS3 plain wordmark"
-          />
+          <div className="skills-icon-container">
+            <img
+              src={require("./imgs/html5-plain-wordmark.svg")}
+              alt="HTML5 plain wordmark"
+            />
+            <img
+              src={require("./imgs/css3-plain-wordmark.svg")}
+              alt="CSS3 plain wordmark"
+            />
+            <img
+              src={require("./imgs/javascript-plain.svg")}
+              alt="JavaScript plain icon"
+            />
+            <img src={require("./imgs/git-plain.svg")} alt="Git plain icon" />
+            <img
+              src={require("./imgs/react-original-wordmark.svg")}
+              alt="React original wordmark"
+            />
+            <img src={require("./imgs/redux-original.svg")} alt="redux icon" />
+            <img
+              src={require("./imgs/sass-original.svg")}
+              alt="sass original icon"
+            />
+          </div>
         </section>
       </main>
+      <footer id="contact">
+        <h4>Contact Me</h4>
+        <p>Edward Estrada 2020</p>
+      </footer>
     </>
   );
 }
